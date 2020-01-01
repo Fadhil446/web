2b1235d2a6f18f2c937ab0f788987e112ddb90a0
Fadhil446 <aizawayu13@gmail.com>
2020-01-01 23:41:59 +0700

    another commit

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 //koneksi ke database mysql, silahkan di rubah dengan koneksi agan sendiri
-$koneksi = mysqli_connect("localhost","root","","web");
+$koneksi = mysqli_connect("us-cdbr-iron-east-05.cleardb.net","b4542555d2d90a","d1b5e0cb","web");
 
 //cek jika koneksi ke mysql gagal, maka akan tampil pesan berikut
 if (mysqli_connect_errno()){
